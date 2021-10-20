52e7a582fb6e501fad8db0d7684d71bfe0e75a66
sindhu21-m <83869499+sindhu21-m@users.noreply.github.com>
2021-10-20 21:00:23 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : black;"><b>BANK OF BARODA</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #C0392B;"><button><b>Home</b></button></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #C0392B;"><button><b>Create User</b></button></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #C0392B;"><button><b>Transfer Money</b></button></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #C0392B;"><button><b>Transaction History</b></button></a>
+              </li>
+          </div>
+       </nav>
